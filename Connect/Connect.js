627960be681c09f18765afacbaed197f90dcae9e
Emeka-code <clintonekegbu@gmail.com>
2022-06-04 01:43:22 +0100

    back

---
+++ Connect/Connect.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const url = "mongodb://localhost/stfcreateDB";
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to databasse");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
